16e556c113964f3a8ec4fb15a0875952f7b19434
Sara <sara_lunden1@hotmail.com>
2025-09-19 14:23:49 +0200

    Wireframe Booking Page

--- src/Pages/BookingPage/bookingPage.tsx
+++ src/Pages/BookingPage/bookingPage.tsx
@@ -1,7 +1,13 @@
+import "../../index.css"
+
 export default function BookingPage() {
     return (
         <>
             <h1>BookingPage</h1>
+
+            <main className="container_box w-105 h-150">
+
+            </main>
         </>
     );
 }
