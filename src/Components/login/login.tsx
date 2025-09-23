9bb5980c4ac7b41a508578da3858dde69ae5db38
Alex Vong <avong4@gmail.com>
2025-09-23 10:30:45 +0200

    updated login component and added register component

--- src/Components/login/login.tsx
+++ src/Components/login/login.tsx
@@ -2,20 +2,21 @@ import { Link } from "react-router";
 
 export default function Login() {
     return (
-        <section className="border p-5">
-            <h1 className="text-4xl">Logga in</h1>
+        <section className="border p-5 flex flex-col">
+            <h1 className="text-4xl">Logga In</h1>
 
             <form className="flex flex-col m-4">
-            <h2 className="my-2">Användarnamn</h2>
-            <input className="my-1" type="text" placeholder="Din Användarnamn" />
+            <h2 className="my-2">E-Post</h2>
+            <input className="my-1 p-2 border" type="text" placeholder="Din E-Post" />
 
             <h2 className="my-2">Lösenord</h2>
-            <input className="my-1" type="password" placeholder="Din Lösenord" />
+            <input className="my-1 p-2 border" type="password" placeholder="Din Lösenord" />
+            <Link to={"/forgot-password"} className="text-xs">Glömt din lösenord?</Link>
 
-            <button>Logga In</button>
             </form>
+            <button className="cursor-pointer my-3 border p-4">Logga In</button>
 
-            <Link to="/register">Har ingen konto? Skapa här</Link>
+            <Link to="/register" className="text-purple-900">Har ingen konto? Skapa här</Link>
         </section>
     );
 }
--- src/Components/register/register.tsx
+++ src/Components/register/register.tsx
@@ -1,5 +1,18 @@
 export default function Register() {
     return (
-        <h1>Register</h1>
+        <section className="border p-5 flex flex-col">
+            <h1 className="text-4xl">Skapa din konto</h1>
+
+            <form className="flex flex-col m-4">
+            <h2 className="my-2">E-Post</h2>
+            <input className="my-1 p-2 border" type="text" placeholder="Din E-Post" />
+
+            <h2 className="my-2">Lösenord</h2>
+            <input className="my-1 p-2 border" type="password" placeholder="Din Lösenord" />
+
+            </form>
+            <button className="cursor-pointer my-3 border p-4">Skapa konto</button>
+
+        </section>
     );
 }
