41a61fd7d280709a122704f6b283f8e2e0c19221
Alex Vong <avong4@gmail.com>
2025-09-23 10:07:25 +0200

    updated login feature

--- src/Components/login/login.tsx
+++ src/Components/login/login.tsx
@@ -2,9 +2,20 @@ import { Link } from "react-router";
 
 export default function Login() {
     return (
-        <>
-            <h1>Logga in</h1>
-            <Link to="/register">Registrera konto</Link>
-        </>
+        <section className="border p-5">
+            <h1 className="text-4xl">Logga in</h1>
+
+            <form className="flex flex-col m-4">
+            <h2 className="my-2">Användarnamn</h2>
+            <input className="my-1" type="text" placeholder="Din Användarnamn" />
+
+            <h2 className="my-2">Lösenord</h2>
+            <input className="my-1" type="password" placeholder="Din Lösenord" />
+
+            <button>Logga In</button>
+            </form>
+
+            <Link to="/register">Har ingen konto? Skapa här</Link>
+        </section>
     );
 }
