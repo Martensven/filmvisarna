e41fc43551a39e8230cae261572982ecee0963ec
Sara <sara_lunden1@hotmail.com>
2025-09-24 15:07:59 +0200

    Style and responsive for bookingpage

--- src/Pages/BookingPage/bookingPage.tsx
+++ src/Pages/BookingPage/bookingPage.tsx
@@ -9,8 +9,8 @@ export default function BookingPage() {
     <>
       
       {/*----------Container for booking page----------*/}
-      <main className="flex flex-col justify-center items-center overscroll-y-auto w-screen min-h-screen bg-[#292929] z-0 md:grid md:grid-rows-1">
-      <h1 className="p-5 text-lg text-white font-bold">Boka din biljett</h1>
+      <main className="flex flex-col justify-center items-center overscroll-x-none overscroll-y-auto w-screen min-h-screen bg-[#292929] md:grid md:grid-rows-1">
+      <h1 className="p-5 text-lg text-[#e4e1e1] font-bold ">Boka din biljett</h1>
        <MovieInformation />
         <TheaterComponent />
         <BookingComponent />
--- src/Pages/BookingPage/components/MovieInformation.tsx
+++ src/Pages/BookingPage/components/MovieInformation.tsx
@@ -1,10 +1,12 @@
+
+
 export default function MovieInformation() {
   return (
 
-    <main className="w-screen md:flex md:flex-row md:w-full md:h-100">
+    <main className="w-screen md:flex md:flex-row md:justify-around md:w-screen md:h-100">
       {/*----------Container for movie poster and title, Genre, age and time----------*/}
-      <section className="flex flex-row items-center justify-center w-full container_box h-76  
-      md:flex md:w-4/5 md:h-86 md:justify-around md:itmes-center md:m-2 
+      <section className="flex flex-row items-center justify-center container_box h-76  
+      md:flex md:w-3/5 md:h-86 md:justify-center md:itmes-center md:m-2 
       lg:w-4/5 lg:h-80">
 
         {/*----------Container for movie poster----------*/}
@@ -13,7 +15,7 @@ export default function MovieInformation() {
           alt="FILM POSTER"
           className="w-50 h-60 m-0.5
            sm:w-70 
-           md:w-3/6 md:h-76 md:m-2 lg:h-100"
+           md:w-2/6 md:h-76 md:m-5 lg:h-100 rounded-sm"
         />
 
         {/*----------Container movie info ----------*/}
@@ -32,13 +34,13 @@ export default function MovieInformation() {
         </article>
       </section>
       {/*----------Container for chosing date and tickets----------*/}
-      <section className="flex flex-col justify-center items-center container_box h-36 w-screen mt-1
+      <section className="flex flex-col justify-center items-center container_box h-36 w-screen
        sm:h-46
-       md:w-auto md:h-65 md:m-4 md:flex md:justify-center md:items-center lg:h-80">
+       md:w-auto md:min-h-86 md:max-h-86 md:m-2 md:flex md:justify-center md:items-center lg:h-80">
         {/*----------Container for chosing the amount of people that need tickets----------*/}
         <article className="flex flex-col items-center w-screen h-34 md:w-auto md:h-65">
           <section className="flex flex-col justify-center w-auto md:flex-col md:justify-center md:items-center md:w-72 md:h-40">
-            <h2 className="text-sm md:text-s">DATUM</h2>
+            <h2 className="text-[#e4e1e1] text-sm md:text-s">DATUM</h2>
 
             {/*----------Containers for calender days----------*/}
             <section className="flex flex-row m-1 container_content w-screen h-15 overflow-x-auto overflow-y-hidden
@@ -74,7 +76,7 @@ export default function MovieInformation() {
           </section>
 
           <section className="flex flex-row justify-center w-40 md:w-72 md:flex-col md:justify-center md:items-center">
-            <h2 className="text-sm md:text-s">ANTAL STOLAR</h2>
+            <h2 className="text-[#e4e1e1] text-sm md:text-s">ANTAL STOLAR</h2>
             <select className="md:w-20 md:h-5"></select>
           </section>
         </article>
