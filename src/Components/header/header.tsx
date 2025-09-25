c39eec02ae4f849a55033293ca4c90833ba14465
[Luckmore83] <[daaksve@gmail.com]>
2025-09-25 13:18:07 +0200

    movie page mockup

--- src/Components/header/header.tsx
+++ src/Components/header/header.tsx
@@ -51,37 +51,7 @@ export default function Header() {
     </main>
   );
 }
-//         <main>
-//             <h1 className="font-bold mt-2 text-white">FILMVISARNA</h1>
-//             <nav className=" rounded-md h-12 mx-5 md:mx-20 mt-10 bg-[#243365] text-white shadow-md">
-//                 <ul className="flex justify-between items-center h-full px-4">
-//                     <li>
-//                         <Link to="/">Start</Link>
-//                     </li>
-//                     <li>
-//                         <Link to="/booking">Boka</Link>
-//                     </li>
-//                     <li>
-//                         <Link to="/movie">Filmer</Link>
-//                     </li>
-//                     <li>
-//                         <Link to="/theme">Temadagar</Link>
-//                     </li>
 
-//                     <li>
-//                         <Link to="/about">Om Oss</Link>
-//                     </li>
-//                     <li>
-//                         <Link to="/kiosk">Kiosk</Link>
-//                     </li>
-//                     <li>
-//                         <Link to="/login">Logga In</Link>
-//                     </li>
-//                 </ul>
-//             </nav>
-//         </main>
-//     );
-// }
 
 
 
