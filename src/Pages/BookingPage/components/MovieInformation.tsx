21e2d643dd43809c62a5ec3e7792391d071ce4c4
Sara <sara_lunden1@hotmail.com>
2025-09-25 10:09:38 +0200

    Style on 375px for movie information container

--- src/Pages/BookingPage/components/MovieInformation.tsx
+++ src/Pages/BookingPage/components/MovieInformation.tsx
@@ -3,10 +3,10 @@
 export default function MovieInformation() {
   return (
 
-    <main className="flex flex-col justify-center items-center w-full 
+    <main className="flex flex-col justify-center items-center w-full
     md:flex md:flex-row md:justify-around md:w-screen md:h-100">
       {/*----------Container for movie poster and title, Genre, age and time----------*/}
-      <section className="flex flex-row items-center justify-center container_box h-80 
+      <section className="flex flex-row items-center justify-center container_box w-100 h-70 mb-5 
       md:flex md:w-3/5 md:h-86 md:justify-center md:itmes-center md:m-2 
       lg:w-4/5 lg:h-80">
 
@@ -14,24 +14,24 @@ export default function MovieInformation() {
         <img
           src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/40/Jaws_movie_poster.jpg/640px-Jaws_movie_poster.jpg"
           alt="FILM POSTER"
-          className="w-40 h-auto rounded-sm
-           sm:w-50 h-auto mr-1
+          className="w-42 h-auto rounded-sm
+           sm:w-50 h-auto mr-2
            md:w-2/6 md:h-auto md:m-5 lg:h-100 "
         />
 
         {/*----------Container movie info ----------*/}
-        <article className="flex flex-col container_content h-auto ml-1
+        <article className="flex flex-col container_content h-62 ml-2
         sm:
         md:w-2/5 md:m-1 
         lg:flex lg:justify-center lg:items-center lg:m-2">
-          <h1 className="flex items-center w-44 h-15 text-left pl-6
+          <h1 className="flex items-center w-44 h-12 text-left pl-6 text-lg
            md:w-45 md:text-xl lg:m-1">
             Jaws
           </h1>
           <ul className=" w-44 h-25 text-left ml-2 md:w-45 md:text-sm lg:m-1">
-            <li className="p-1">År:</li>
-            <li className="p-1">Genre:</li>
-            <li className="p-1">Handling:</li>
+            <li className="text-xs p-0.5">År: 1975</li>
+            <li className="text-xs p-0.5">Genre: Thriller/Horror</li>
+            <li className="text-xs pt-2">”När en jättelik vithaj dödligt attackerar simmare vid Amity Islands stränder slår sig sheriff Martin Brody ihop med en marinbiolog och en lokal fiskare för att jaga varelsen.”</li>
             </ul>
         </article>
       </section>
