57428bc035fb5295a3bda9210f0dd40dbd38094e
Jesper Eriksson Stenberg <jespermail@gmail.com>
2025-09-25 11:46:19 +0200

    Added mockdata for kiosk items

---
+++ Backend/kiosk.ts
@@ -0,0 +1,16 @@
+// Sample data for kiosk items
+
+const kiosklist = [
+    { id: 1, name: "Cola", price: 25, category: "drink", size: "50cl", image: "../public/images/kiosk/cocacola.webp" },
+    { id: 2, name: "Fanta", price: 25, category: "drink", size: "50cl", image: "../public/images/kiosk/fanta.webp" },
+    { id: 3, name: "Ramlösa", price: 20, category: "drink", size: "50cl", image: "../public/images/kiosk/ramlosa.webp" },
+    { id: 4, name: "Festis", price: 15, category: "drink", size: "200ml", image: "../public/images/kiosk/festis.webp" },
+    { id: 5, name: "Popcorn", price: 30, category: "snack", size: "200g", image: "../public/images/kiosk/popcorn.webp" },
+    { id: 6, name: "Lättsaltade chips", price: 35, category: "snack", size: "200g", image: "../public/images/kiosk/chips.webp" },
+    { id: 7, name: "Baconsnacks", price: 40, category: "snack", size: "300g", image: "../public/images/kiosk/baconsnacks.webp" },
+    { id: 8, name: "Ostbågar", price: 35, category: "snack", size: "200g", image: "../public/images/kiosk/ostbagar.webp" },
+    { id: 9, name: "Gott & Blandat", price: 25, category: "candy", size: "100g", image: "../public/images/kiosk/gottoblandat.webp" },
+    { id: 10, name: "Chokladkaka", price: 30, category: "candy", size: "100g", image: "../public/images/kiosk/chokladkaka.webp" },
+    { id: 11, name: "Sura nappar", price: 20, category: "candy", size: "100g", image: "../public/images/kiosk/suranappar.webp" },
+    { id: 12, name: "nappar", price: 20, category: "candy", size: "100g", image: "../public/images/kiosk/nappar.webp" },
+]
