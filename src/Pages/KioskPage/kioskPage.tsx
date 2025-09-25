e65b21aa7a36a93ee98601ac3ba7c811aa767f9b
Jesper Eriksson Stenberg <jespermail@gmail.com>
2025-09-25 22:18:09 +0200

    Working on fetching mock data dynamically from kiosk.ts

--- src/Pages/KioskPage/kioskPage.tsx
+++ src/Pages/KioskPage/kioskPage.tsx
@@ -1,5 +1,6 @@
 import kiosklist from "../../../Backend/kiosk";
 import type { KioskItem } from "../../../Backend/kiosk";
+import CategorySection from "./components/categorySection";
 
 export default function KioskPage() {
 
@@ -12,10 +13,19 @@ export default function KioskPage() {
       <header className="text-center">
         <h1 className="text-2xl mt-4">Kiosk</h1>
         <h2 className="text-xl">Vårt utbud i kiosken</h2>
+
+        <CategorySection title="Dryck" items={drinks} />
+        <CategorySection title="Snacks" items={snacks} />
+        <CategorySection title="Godis" items={candy} />
       </header>
     </main>
-  )
+  );
 }
+
+export type CategorySectionProps = {
+  title: string;
+  items: KioskItem[];
+};
 
 
 // export default function KioskPage() {
