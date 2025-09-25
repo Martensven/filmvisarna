6b056a56c8bd9f479da57dbae807ed40ab8c3551
Martensven <martengullstrom@gmail.com>
2025-09-25 13:59:13 +0200

    added to mockDB

--- Backend/example.ts
+++ Backend/example.ts
@@ -5,7 +5,27 @@ const exampleList = [
         image: "/images/Casablanca.jpg",
         genre: ["Drama", "Romantik"],
         releaseYear: 1942,
-        age: 15
+        age: 15,
+        length: 102,
+        distributor: "Warner Bros",
+        director: "Michael Curtiz",
+        actors: ["Humphrey Bogart", "Ingrid Bergman", "Paul Henreid"],
+        description: "En cynisk amerikansk expatriat i Casablanca måste välja mellan sin kärlek till en kvinna och att hjälpa hennes och hennes motståndsledare make att fly från nazisterna.",
+        "youtubeTrailers": [
+            "Z9AYPxH5NTM"
+        ], "reviews": [
+            {
+                reviewer: "Filmkritiker 1",
+                comment: "En tidlös klassiker som fortfarande håller.",
+                rating: 5
+            },
+            {
+                reviewer: "Filmkritiker 2",
+                comment: "Fantastiska prestationer av skådespelarna.",
+                rating: 4
+            }
+        ],
+        themeDay: "any",
     },
     {
         id: 2,
@@ -13,7 +33,27 @@ const exampleList = [
         image: "/images/Citizen-kane.jpg",
         genre: ["Drama", "Mystik"],
         releaseYear: 1941,
-        age: 15
+        age: 15,
+        length: 119,
+        distributor: "RKO Radio Pictures",
+        director: "Orson Welles",
+        actors: ["Orson Welles", "Joseph Cotten", "Dorothy Comingore"],
+        description: "En undersökning av livet och arvet efter en publiceringsmagnat avslöjar komplexiteten",
+        "youtubeTrailers": [
+            "YQe3yXg-s1c"
+        ], "reviews": [
+            {
+                reviewer: "Filmkritiker 1",
+                comment: "En djupgående och tankeväckande film.",
+                rating: 5
+            },
+            {
+                reviewer: "Filmkritiker 2",
+                comment: "En mästerlig prestation av Orson Welles.",
+                rating: 4
+            }
+        ],
+        themeDay: "any",
     },
     {
         id: 3,
@@ -21,7 +61,27 @@ const exampleList = [
         image: "/images/die-hard.webp",
         genre: ["Action"],
         releaseYear: 1988,
-        age: 15
+        age: 15,
+        length: 132,
+        distributor: "20th Century Fox",
+        director: "John McTiernan",
+        actors: ["Bruce Willis", "Alan Rickman", "Bonnie Bedelia"],
+        description: "En polisman måste rädda gisslan i en skyskrapa som har tagits över av terrorister.",
+        "youtubeTrailers": [
+            "Q0m1g1g1g1g"
+        ], "reviews": [
+            {
+                reviewer: "Filmkritiker 1",
+                comment: "En actionfylld klassiker som fortfarande imponerar.",
+                rating: 5
+            },
+            {
+                reviewer: "Filmkritiker 2",
+                comment: "Bruce Willis är fantastisk i huvudrollen.",
+                rating: 4
+            }
+        ],
+        themeDay: "any",
     },
     {
         id: 4,
@@ -29,7 +89,27 @@ const exampleList = [
         image: "/images/ghost-busters.webp",
         genre: ["Komedi"],
         releaseYear: 1984,
-        age: 11
+        age: 11,
+        length: 105,
+        distributor: "Columbia Pictures",
+        director: "Ivan Reitman",
+        actors: ["Bill Murray", "Dan Aykroyd", "Sigourney Weaver"],
+        description: "Ett team av parapsykologer startar en spökjakt i New York City.",
+        "youtubeTrailers": [
+            "K1j1g1g1g1g"
+        ], "reviews": [
+            {
+                reviewer: "Filmkritiker 1",
+                comment: "En rolig och underhållande film.",
+                rating: 5
+            },
+            {
+                reviewer: "Filmkritiker 2",
+                comment: "Fantastiska prestationer av skådespelarna.",
+                rating: 4
+            }
+        ],
+        themeDay: "any",
     },
     {
         id: 5,
@@ -37,7 +117,27 @@ const exampleList = [
         image: "/images/Jurassic-park.webp",
         genre: "Äventyr",
         releaseYear: 1993,
-        age: 11
+        age: 11,
+        length: 127,
+        distributor: "Universal Pictures",
+        director: "Steven Spielberg",
+        actors: ["Sam Neill", "Laura Dern", "Jeff Goldblum"],
+        description: "En nöjespark med klonade dinosaurier går fruktansvärt fel när säkerhetssystemen slås ut.",
+        youtubeTrailers: [
+            "jNQXAC9IVRw"
+        ], reviews: [
+            {
+                reviewer: "Filmkritiker 1",
+                comment: "En fantastisk filmupplevelse med banbrytande effekter.",
+                rating: 5
+            },
+            {
+                reviewer: "Filmkritiker 2",
+                comment: "En spännande och välgjord film.",
+                rating: 4
+            }
+        ],
+        themeDay: "any"
     },
     {
         id: 6,
@@ -45,7 +145,27 @@ const exampleList = [
         image: "/images/jaws.webp",
         genre: ["Skräck", "Äventyr", "Thriller"],
         releaseYear: 1975,
-        age: 15
+        age: 15,
+        length: 124,
+        distributor: "Universal Pictures",
+        director: "Steven Spielberg",
+        actors: ["Roy Scheider", "Robert Shaw", "Richard Dreyfuss"],
+        description: "En stor vit haj terroriserar en strandstad, och en grupp män måste stoppa den.",
+        youtubeTrailers: [
+            "U1j1g1g1g1g"
+        ], reviews: [
+            {
+                reviewer: "Filmkritiker 1",
+                comment: "En klassiker inom skräckgenren.",
+                rating: 5
+            },
+            {
+                reviewer: "Filmkritiker 2",
+                comment: "Spännande och välgjord film.",
+                rating: 4
+            }
+        ],
+        themeDay: "any"
     }
 ]
 
