cbed852a6a8449c492c8a5c4d71923d06a588b3d
Jesper Eriksson Stenberg <jespermail@gmail.com>
2025-09-25 22:07:45 +0200

    Working on fetching mock data dynamically from kiosk.ts

--- src/Pages/KioskPage/kioskPage.tsx
+++ src/Pages/KioskPage/kioskPage.tsx
@@ -1,4 +1,21 @@
+import kiosklist from "../../../Backend/kiosk";
+import type { KioskItem } from "../../../Backend/kiosk";
 
+export default function KioskPage() {
+
+  const drinks: KioskItem[] = kiosklist.filter((item) => item.category === "drink");
+  const snacks: KioskItem[] = kiosklist.filter((item) => item.category === "snack");
+  const candy: KioskItem[] = kiosklist.filter((item) => item.category === "candy");
+
+  return (
+    <main className="w-screen bg-[#292929] text-white">
+      <header className="text-center">
+        <h1 className="text-2xl mt-4">Kiosk</h1>
+        <h2 className="text-xl">Vårt utbud i kiosken</h2>
+      </header>
+    </main>
+  )
+}
 
 
 // export default function KioskPage() {
