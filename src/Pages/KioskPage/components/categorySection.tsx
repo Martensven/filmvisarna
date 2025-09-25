7949838d1e191a6e9a28d90a8c9279764edef3ea
Jesper Eriksson Stenberg <jespermail@gmail.com>
2025-09-25 22:48:12 +0200

    Working on CategorySection, img functionality added

--- src/Pages/KioskPage/components/categorySection.tsx
+++ src/Pages/KioskPage/components/categorySection.tsx
@@ -1,10 +1,26 @@
 import type { CategorySectionProps } from "../kioskPage";
 
-export default function CategorySection({ title, items }: CategorySectionProps) {
-
-    return (
-        <section className="w-screen flex flex-wrap justify-center items-center">
-            <h3 className="w-full text-center text-xl m-4">{title}</h3>
-        </section>
-    );
+export default function CategorySection({
+  title,
+  items,
+}: CategorySectionProps) {
+  return (
+    <section className="w-screen flex flex-wrap justify-center items-center">
+      <h3 className="text-center text-xl m-4">{title}</h3>
+      <section className="w-full flex flex-wrap justify-center items-center gap-5">
+        {items.map((item) => (
+          <article
+            key={item.id}
+            className="w-48 h-64 relative flex items-end justify-center text-center text-white"
+          >
+            <img
+              src={item.image}
+              alt={item.name}
+              className="absolute inset-0 w-full h-full object-cover hover:rotate-10 transition-all duration-300"
+            />
+          </article>
+        ))}
+      </section>
+    </section>
+  );
 }
