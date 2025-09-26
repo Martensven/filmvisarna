e842bde0379b8692cd5ce0204e469cca1345c434
Sara <sara_lunden1@hotmail.com>
2025-09-26 10:59:54 +0200

    ticket counter visually fixed

--- src/Pages/BookingPage/components/SeatsTheater.tsx
+++ src/Pages/BookingPage/components/SeatsTheater.tsx
@@ -1,19 +1,22 @@
-import {Link} from "react"
-import TheaterView from "./TheaterView";
+
+import { useState} from "react"
 
 export default function SeatsTheater() {
-const seats = [1,2,3,4,5,6,7,8,9,10,11,12]
-
+const [countTickets, setCountTickets] = useState<number>(1); //Always a start with 1
+const increment:React.MouseEventHandler<HTMLButtonElement> = () => {setCountTickets(prev => prev + 1);}
+const decrement:React.MouseEventHandler<HTMLButtonElement> = () => {setCountTickets(prev => (prev > 0 ? prev - 1:0));} //decrement but prevents a number under 0
 
   return (
     <main className="flex flex-col justify-center items-center w-86">
+      <div className="flex flex-row justify-between w-72">
       <h2 className="text-[#e4e1e1] text-s m-2 md:text-s">Biljetter</h2>
       <section className="flex flex-row justify-content items-center">
-        <button className="seat_range_buttons w-7 h-7 mr-5 align">-</button>
-        <p className="text-m ml-2 mr-2">1</p>
-        <button className="seat_range_buttons w-7 h-7 ml-5">+</button>
+        <button className="seat_range_buttons w-7 h-7 mr-5 align cursor-pointer" onClick={decrement} aria-label="Minska biljetter">-</button>
+        <p className="text-m ml-2 mr-2">{countTickets}</p>
+        <button className="seat_range_buttons w-7 h-7 ml-5 cursor-pointer" onClick={increment} aria-label="Öka biljetter">+</button>
       </section>
-      <button className="main_buttons w-36 m-4">
+      </div>
+      <button className="main_buttons w-36 m-4 cursor-pointer">
         Välj
       </button>
     </main>
