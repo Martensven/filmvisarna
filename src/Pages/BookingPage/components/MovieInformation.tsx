5939e15dd70fdd8a9232a6cc781e025109769e21
Sara <sara_lunden1@hotmail.com>
2025-09-30 14:33:44 +0200

    style improvments

--- src/Pages/BookingPage/components/MovieInformation.tsx
+++ src/Pages/BookingPage/components/MovieInformation.tsx
@@ -10,7 +10,7 @@ export default function MovieInformation() {
       {/*----------Container for movie poster and title, Genre, age and time----------*/}
       <section
         className="flex flex-row container_box w-11/12 h-auto mb-5
-                   sm:w-4/5 
+                   sm:w-11/12
                    md:w-100 md:h-auto md:ml-3 
                    lg:h-80"
       >
@@ -25,23 +25,28 @@ export default function MovieInformation() {
 
         {/*----------Container movie info ----------*/}
         <article
-          className="flex flex-col justify-start W-5/12 h-auto  text-[#e4e1e1]
-                     sm:w-2/6
+          className="flex flex-col justify-start w-6/12 h-auto text-[#e4e1e1]
+                    sm:w-6/12 sm:h-auto
                      md:w-3/6 md:h-auto 
                      lg:flex lg:justify-center lg:items-center lg:m-2"
         >
           <h1
-            className="flex w-5/12 h-auto text-left mt-3 text-lg
-                       md:justify-start md:w-full md:text-3xl md:underline md:mt-10 lg:m-1"
+            className="flex w-5/12 h-auto text-left mt-3 text-lg underline
+                      sm:text-xl sm:m-3 sm:h-auto
+                       md:justify-start md:w-full md:text-3xl md:mt-10 lg:m-1"
           >
             Jaws
           </h1>
-          <ul className="w-full h-25 text-left md:w-full md:h-auto md:text-md lg:m-1">
+          <ul className="w-full h-25 text-left
+                        sm:text-base sm:ml-3 sm:h-auto
+                        md:w-full md:h-auto md:text-md lg:m-1">
             <li className="text-xs italic md:text-sm">År: 1975</li>
             <li className="text-xs italic md:text-sm">
               Genre: Thriller/Horror
             </li>
-            <li className="w-full h-auto text-xs pt-4 text-center md:text-start md:mt-5 md:text-sm ">
+            <li className="h-auto text-xs pt-4 text-center
+                          sm:text-base sm:text-start sm:w-50 sm:h-auto sm:pt-2 sm:mt-10
+                          md:text-start md:mt-5 md:text-sm ">
               ”När en jättelik vithaj dödligt attackerar simmare vid Amity
               Islands stränder slår sig sheriff Martin Brody ihop med en
               marinbiolog och en lokal fiskare för att jaga varelsen.”
