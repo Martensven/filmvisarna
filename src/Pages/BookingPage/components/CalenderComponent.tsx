b46d73c4ab1be05abe1a7f3a68562503610089e9
Jesper Eriksson Stenberg <jespermail@gmail.com>
2025-10-01 14:54:55 +0200

    CalenderComponent.tsx, ability to click. Seting state to show theater.

--- src/Pages/BookingPage/components/CalenderComponent.tsx
+++ src/Pages/BookingPage/components/CalenderComponent.tsx
@@ -1,7 +1,11 @@
 import "../BookingPageStyle.css";
 //Component contains todays showing and other dates. This ones are going to be clickable
-export default function CalenderComponent() {
 
+interface Props {
+  onSelectTheater: (theater: string) => void;
+}
+
+export default function CalenderComponent({ onSelectTheater }: Props) {
   return (
     <main className="h-auto flex flex-col justify-center items-center md:w-11/12">
       {/*----------Containers for calender days----------*/}
@@ -10,19 +14,31 @@ export default function CalenderComponent() {
         className="flex flex-row container_content w-11/12 mb-1 overflow-x-auto overflow-y-hidden
          md:w-full md:place-items-center"
       >
-        <ul className="container_box calenderDatesContainer w-36 md:w-4/5 md:h-30 md:text-xs">
+        <ul
+          onClick={() => onSelectTheater("Stora Salongen")}
+          className="container_box calenderDatesContainer w-36 md:w-4/5 md:h-30 md:text-xs"
+        >
           <li className="pt-3 pb-3 text-lg font-bold">12:00</li>
           <li className="pb-1 text-sm md:text-md">Stora Salongen</li>
         </ul>
-        <ul className="container_box calenderDatesContainer w-36 md:w-4/5 md:h-30 md:text-xs">
+        <ul
+          onClick={() => onSelectTheater("Stora Salongen")}
+          className="container_box calenderDatesContainer w-36 md:w-4/5 md:h-30 md:text-xs"
+        >
           <li className="pt-3 pb-3 text-lg font-bold">14:00</li>
           <li className="pb-1 text-sm">Stora Salongen</li>
         </ul>
-        <ul className="container_box calenderDatesContainer w-36 md:w-4/5 md:h-30 md:text-xs">
+        <ul
+          onClick={() => onSelectTheater("Lilla Salongen")}
+          className="container_box calenderDatesContainer w-36 md:w-4/5 md:h-30 md:text-xs"
+        >
           <li className="pt-3 pb-3 text-lg font-bold">18:00</li>
           <li className="pb-1 text-sm">Lilla Salongen</li>
         </ul>
-        <ul className="container_box calenderDatesContainer w-36 md:w-4/5 md:h-30 md:text-xs">
+        <ul
+          onClick={() => onSelectTheater("Lilla Salongen")}
+          className="container_box calenderDatesContainer w-36 md:w-4/5 md:h-30 md:text-xs"
+        >
           <li className="pt-3 pb-3 text-lg font-bold">20:00</li>
           <li className="pb-1 text-sm">Lilla Salongen</li>
         </ul>
@@ -36,48 +52,65 @@ export default function CalenderComponent() {
         sm:w-11/12
         md:w-full md:h-auto"
       >
-          <ul  className="container_box calenderDatesContainer w-24 
+        <ul
+          onClick={() => onSelectTheater("Stora Salongen")}
+          className="container_box calenderDatesContainer w-24 
                           sm:w-32
-                          md:w-20 md:h-32 md:text-xs">
-            <li className="pt-1 pb-2 text-md font-bold">Torsdag 02/10</li>
-            <li className="text-md">15:00</li>
-            <li className="pt-3 text-sm font-bold">Stora Salongen</li>
-          </ul>
-          <ul className="container_box calenderDatesContainer w-24 
+                          md:w-20 md:h-32 md:text-xs"
+        >
+          <li className="pt-1 pb-2 text-md font-bold">Torsdag 02/10</li>
+          <li className="text-md">15:00</li>
+          <li className="pt-3 text-sm font-bold">Stora Salongen</li>
+        </ul>
+        <ul
+          onClick={() => onSelectTheater("Stora Salongen")}
+          className="container_box calenderDatesContainer w-24 
                         sm:w-32
-                        md:w-20 md:h-32 md:text-xs">
-            <li className="pt-1 pb-2 text-md font-bold">Torsdag 02/10</li>
-            <li className="text-md">20:00</li>
-            <li className="pt-3 text-sm font-bold">Stora Salongen</li>
-          </ul>
-          <ul className="container_box calenderDatesContainer w-24 
+                        md:w-20 md:h-32 md:text-xs"
+        >
+          <li className="pt-1 pb-2 text-md font-bold">Torsdag 02/10</li>
+          <li className="text-md">20:00</li>
+          <li className="pt-3 text-sm font-bold">Stora Salongen</li>
+        </ul>
+        <ul
+          onClick={() => onSelectTheater("Stora Salongen")}
+          className="container_box calenderDatesContainer w-24 
                         sm:w-32  
-                        md:w-20 md:h-32 md:text-xs">
-            <li className="pt-1 pb-2 text-md font-bold">Söndag 05/10</li>
-            <li className="text-md">15:00</li>
-            <li className="pt-3 text-sm font-bold">Stora Salongen</li>
-          </ul>
-          <ul className="container_box calenderDatesContainer w-24 
+                        md:w-20 md:h-32 md:text-xs"
+        >
+          <li className="pt-1 pb-2 text-md font-bold">Söndag 05/10</li>
+          <li className="text-md">15:00</li>
+          <li className="pt-3 text-sm font-bold">Stora Salongen</li>
+        </ul>
+        <ul
+          onClick={() => onSelectTheater("Lilla Salongen")}
+          className="container_box calenderDatesContainer w-24 
                         sm:w-32
-                        md:w-20 md:h-32 md:text-xs">
-            <li className="pt-1 pb-2 text-md font-bold">Söndag 05/10 </li>
-            <li className="text-md">20:00</li>
-            <li className="pt-3 text-sm font-bold">Lilla Salongen</li>
-          </ul>
-          <ul className="container_box calenderDatesContainer w-24 
+                        md:w-20 md:h-32 md:text-xs"
+        >
+          <li className="pt-1 pb-2 text-md font-bold">Söndag 05/10 </li>
+          <li className="text-md">20:00</li>
+          <li className="pt-3 text-sm font-bold">Lilla Salongen</li>
+        </ul>
+        <ul
+          onClick={() => onSelectTheater("Lilla Salongen")}
+          className="container_box calenderDatesContainer w-24 
                         sm:w-32
-                        md:w-20 md:h-32 md:text-xs">
-            <li className="pt-1 pb-2 text-md font-bold">Onsdag 08/10</li>
-            <li className="text-md">12:00</li>
-            <li className="pt-3 text-sm font-bold">Lilla Salongen</li>
-          </ul>
-          <ul className="container_box calenderDatesContainer w-24 
+                        md:w-20 md:h-32 md:text-xs"
+        >
+          <li className="pt-1 pb-2 text-md font-bold">Onsdag 08/10</li>
+          <li className="text-md">12:00</li>
+          <li className="pt-3 text-sm font-bold">Lilla Salongen</li>
+        </ul>
+        <ul
+          className="container_box calenderDatesContainer w-24 
                         sm:w-32
-                        md:w-20 md:h-32 md:text-xs">
-            <li className="pt-1 pb-2 text-md font-bold">Onsdag 08/10</li>
-            <li className="text-md">18:00</li>
-            <li className="pt-3 text-sm font-bold">Stora Salongen</li>
-          </ul>
+                        md:w-20 md:h-32 md:text-xs"
+        >
+          <li className="pt-1 pb-2 text-md font-bold">Onsdag 08/10</li>
+          <li className="text-md">18:00</li>
+          <li className="pt-3 text-sm font-bold">Stora Salongen</li>
+        </ul>
       </section>
     </main>
   );
