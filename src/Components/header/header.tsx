689dd689da6fb7951eb3c894ee643ac8eaf47550
Sara <sara_lunden1@hotmail.com>
2025-10-02 13:39:49 +0200

    removed boka and film from nav

--- src/Components/header/header.tsx
+++ src/Components/header/header.tsx
@@ -42,8 +42,6 @@ export default function Header({ onLoginClick }: HeaderProps) {
                         {/* Desktop menu */}
                         <ul className="desktopNav hidden md:flex w-full justify-between items-center text-sm font-medium">
                             <li><Link to="/"></Link></li>
-                            <li><Link to="/booking">Boka</Link></li>
-                            <li><Link to="/movie">Filmer</Link></li>
                             <li><Link to="/about">Om Oss</Link></li>
                             <li><Link to="/kiosk">Kiosk</Link></li>
                             <li><button onClick={onLoginClick} className="cursor-pointer">Logga In</button></li>
