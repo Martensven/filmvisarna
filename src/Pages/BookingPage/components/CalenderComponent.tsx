fc4fd7e5894cf8ec91061253d7dcd1c680ec7c77
Jesper Eriksson Stenberg <jespermail@gmail.com>
2025-10-02 11:18:33 +0200

    added cursor-pointer

--- src/Pages/BookingPage/components/CalenderComponent.tsx
+++ src/Pages/BookingPage/components/CalenderComponent.tsx
@@ -21,7 +21,7 @@ export default function CalenderComponent({ onSelectTheater }: Props) {
           // Setting active state to mark selected calender date and onSelectTheater to get the selected theater
           // This will be used dynamically later on
           onClick={() => { setActive(1); onSelectTheater("Stora Salongen")}}
-          className={`container_box calenderDatesContainer w-36 md:w-4/5 md:h-30 md:text-xs
+          className={`container_box calenderDatesContainer w-36 md:w-4/5 md:h-30 md:text-xs cursor-pointer
           ${active === 1 ? "!border-4 !border-[#07ca00]" : ""}`}
         >
           
@@ -30,7 +30,7 @@ export default function CalenderComponent({ onSelectTheater }: Props) {
         </ul>
         <ul
           onClick={() => { setActive(2); onSelectTheater("Stora Salongen")}}
-          className={`container_box calenderDatesContainer w-36 md:w-4/5 md:h-30 md:text-xs
+          className={`container_box calenderDatesContainer w-36 md:w-4/5 md:h-30 md:text-xs cursor-pointer
           ${active === 2 ? "!border-4 !border-[#07ca00]" : ""}`}
         >
           <li className="pt-3 pb-3 text-lg font-bold">14:00</li>
@@ -38,7 +38,7 @@ export default function CalenderComponent({ onSelectTheater }: Props) {
         </ul>
         <ul
           onClick={() => { setActive(3); onSelectTheater("Lilla Salongen")}}
-          className={`container_box calenderDatesContainer w-36 md:w-4/5 md:h-30 md:text-xs
+          className={`container_box calenderDatesContainer w-36 md:w-4/5 md:h-30 md:text-xs cursor-pointer
           ${active === 3 ? "!border-4 !border-[#07ca00]" : ""}`}
         >
           <li className="pt-3 pb-3 text-lg font-bold">18:00</li>
@@ -46,7 +46,7 @@ export default function CalenderComponent({ onSelectTheater }: Props) {
         </ul>
         <ul
           onClick={() => { setActive(4); onSelectTheater("Lilla Salongen")}}
-          className={`container_box calenderDatesContainer w-36 md:w-4/5 md:h-30 md:text-xs
+          className={`container_box calenderDatesContainer w-36 md:w-4/5 md:h-30 md:text-xs cursor-pointer
           ${active === 4 ? "!border-4 !border-[#07ca00]" : ""}`}
         >
           <li className="pt-3 pb-3 text-lg font-bold">20:00</li>
@@ -69,6 +69,7 @@ export default function CalenderComponent({ onSelectTheater }: Props) {
           className={`container_box calenderDatesContainer w-24 
                           sm:w-32
                           md:w-20 md:h-32 md:text-xs
+                          cursor-pointer
                           ${active === 5 ? "!border-4 !border-[#07ca00]" : ""}`}
         >
           <li className="pt-1 pb-2 text-md font-bold">Torsdag 02/10</li>
@@ -80,6 +81,7 @@ export default function CalenderComponent({ onSelectTheater }: Props) {
           className={`container_box calenderDatesContainer w-24 
                         sm:w-32
                         md:w-20 md:h-32 md:text-xs
+                        cursor-pointer
                         ${active === 6 ? "!border-4 !border-[#07ca00]" : ""}`}
         >
           <li className="pt-1 pb-2 text-md font-bold">Torsdag 02/10</li>
@@ -91,6 +93,7 @@ export default function CalenderComponent({ onSelectTheater }: Props) {
           className={`container_box calenderDatesContainer w-24 
                         sm:w-32  
                         md:w-20 md:h-32 md:text-xs
+                        cursor-pointer
                         ${active === 7 ? "!border-4 !border-[#07ca00]" : ""}`}
         >
           <li className="pt-1 pb-2 text-md font-bold">Söndag 05/10</li>
@@ -102,6 +105,7 @@ export default function CalenderComponent({ onSelectTheater }: Props) {
           className={`container_box calenderDatesContainer w-24 
                         sm:w-32
                         md:w-20 md:h-32 md:text-xs
+                        cursor-pointer
                         ${active === 8 ? "!border-4 !border-[#07ca00]" : ""}`}
         >
           <li className="pt-1 pb-2 text-md font-bold">Söndag 05/10 </li>
@@ -113,6 +117,7 @@ export default function CalenderComponent({ onSelectTheater }: Props) {
           className={`container_box calenderDatesContainer w-24 
                         sm:w-32
                         md:w-20 md:h-32 md:text-xs
+                        cursor-pointer
                         ${active === 9 ? "!border-4 !border-[#07ca00]" : ""}`}
         >
           <li className="pt-1 pb-2 text-md font-bold">Onsdag 08/10</li>
@@ -124,6 +129,7 @@ export default function CalenderComponent({ onSelectTheater }: Props) {
           className={`container_box calenderDatesContainer w-24 
                         sm:w-32
                         md:w-20 md:h-32 md:text-xs
+                        cursor-pointer
                         ${active === 10 ? "!border-4 !border-[#07ca00]" : ""}`}
         >
           <li className="pt-1 pb-2 text-md font-bold">Onsdag 08/10</li>
