85125e9fd296a5596a6049c243045c9ef552b5be
Sara <sara_lunden1@hotmail.com>
2025-10-02 14:45:43 +0200

    added some chairs for funktionshindrade

--- src/Pages/BookingPage/components/UnavailableSeats.tsx
+++ src/Pages/BookingPage/components/UnavailableSeats.tsx
@@ -1,8 +1,13 @@
 import { useState } from "react"
+import TheaterView from "./TheaterView";
 
 
 export default function UnavailableSeats() {
 const [booked, setBooked] = useState(false);
+
+if(!booked) {
+    <TheaterView />
+}
 
 
     return(
