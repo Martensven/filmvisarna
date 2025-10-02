1c2bd558d0c0bf6a040b14072968f47a4d3ce951
Sara <sara_lunden1@hotmail.com>
2025-10-02 13:47:32 +0200

    created new component for booked chairs

---
+++ src/Pages/BookingPage/components/UnavailableSeats.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react"
+
+
+export default function UnavailableSeats() {
+const [booked, setBooked] = useState(false);
+
+
+    return(
+        <section>
+        </section>
+    )
+};
