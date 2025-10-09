e702412ad126110272536259d549a02aa78545a8
Martensven <martengullstrom@gmail.com>
2025-10-09 14:04:47 +0200

    added bookingSchema

---
+++ Backend/models/bookingSchema.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const bookingSchema = new mongoose.Schema({
+    // If user is logged in, reference their userId. If not, store as null and use guest info.
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: false, default: null },
+    userInfo: {
+        firstName: { type: String, default: "Guest" },
+        lastName: { type: String, default: "Guest" },
+        email: { type: String, default: "Guest" },
+    },
+    // screening info
+    screeningId: { type: mongoose.Schema.Types.ObjectId, ref: 'Screening', required: false, default: null },
+    screeningInfo: {
+        movieTitle: { type: String, required: true },
+        auditoriumName: { type: String, required: true },
+        date: { type: String, required: true },
+        time: { type: String, required: true },
+    },
+    // seat info
+    seatId: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Seat', required: true }],
+    seatInfo: [{ type: Number, required: true }],
+    // ticket info
+    ticketId: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Ticket', required: true }],
+    ticketType: [{ type: Number, required: true }],
+    // total price
+    totalPrice: { type: Number, required: true },
+    // booking timestamp
+    created_at: { type: Date, default: Date.now }
+});
+
+export const Booking = mongoose.model('Booking', bookingSchema);
