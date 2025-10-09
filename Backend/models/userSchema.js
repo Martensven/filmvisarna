54b82993b36290e3e4b7c54b121635753ab99565
Emil Asikainen <emilasikainen@hotmail.com>
2025-10-09 10:40:21 +0200

    edited userSchema.js with email requiring unique id and added lastName as a string

--- Backend/models/userSchema.js
+++ Backend/models/userSchema.js
@@ -2,9 +2,10 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
     userId: { type: Number },
-    email: { type: String },
+    email: { type: String, required: true, unique: true },
     password: { type: String },
     firstName: { type: String },
+    lastName: { type: String },
     phoneNumber: { type: Number },
 });
  
