646c34cbdf223364b2dcb550c8524c752df24045
Alex Vong <avong4@gmail.com>
2025-10-10 09:59:29 +0200

    removed id from schemas

--- Backend/models/moviesSchema.js
+++ Backend/models/moviesSchema.js
@@ -1,7 +1,6 @@
 import mongoose from "mongoose";
 
 const moviesSchema = new mongoose.Schema({
-    movieId: { type: Number },
     title: { type: String },
     imageSrc: { type: String },
     releaseYear: { type: Number },
--- Backend/models/seatSchema.js
+++ Backend/models/seatSchema.js
@@ -1,7 +1,6 @@
 import mongoose from "mongoose";
 
 const seatSchema = new mongoose.Schema({
-    seatId: { type: Number },
     rowNumber: { type: Number },
     seatNumber: { type: Number },
     auditoriumId: { type: Number }
