701e122b4690d62384bab4872de11499c144686c
Martensven <martengullstrom@gmail.com>
2025-10-10 10:07:45 +0200

    added routes to ticketTypes

--- Backend/models/ticketTypeSchema.js
+++ Backend/models/ticketTypeSchema.js
@@ -1,7 +1,6 @@
 import mongoose from "mongoose";
 
 const ticketTypeSchema = new mongoose.Schema({
-    ticketTypeId: { type: Number, required: true },
     ticketName: { type: String, required: true },
     price: { type: Number, required: true }
 });
