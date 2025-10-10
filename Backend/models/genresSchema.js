22800f67661691f9db82a6e47a2b668359726bf5
[Luckmore83] <[daaksve@gmail.com]>
2025-10-10 09:32:44 +0200

    removed genreId because of redundancy

--- Backend/models/genresSchema.js
+++ Backend/models/genresSchema.js
@@ -1,7 +1,6 @@
 import mongoose from "mongoose";
 
 const genresSchema = new mongoose.Schema({
-    genreId: { type: Number, required: true },
     title: { type: String, required: true },
     desc: { type: String/Text, required: true}
 },)
