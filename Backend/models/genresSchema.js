f42855d79328d335e16529c0b62e7b4c61dbaea8
[Luckmore83] <[daaksve@gmail.com]>
2025-10-13 11:09:10 +0200

    another correction

--- Backend/models/genresSchema.js
+++ Backend/models/genresSchema.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const genresSchema = new mongoose.Schema({
     title: { type: String, required: true },
-    desc: { type: Text, required: true}
+    desc: { type: String, required: true}
 },)
 
 export const Genres = mongoose.model('Genres', genresSchema)
