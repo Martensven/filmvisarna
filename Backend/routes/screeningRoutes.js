dec1ae56d781e50451d95d7b9202774b0dd9ebc9
Sara <sara_lunden1@hotmail.com>
2025-10-13 11:22:24 +0200

    new pull of dev

--- Backend/routes/screeningRoutes.js
+++ Backend/routes/screeningRoutes.js
@@ -1,23 +1,38 @@
 import express from "express"
-import { Screening } from "../models/screeningSchema"
+import { Screening } from "../models/screeningSchema.js";
 
-const route = express();
+const router = express();
 
-route.post("/api/screening", async (req, res) => {
+router.get("/api/screening", async (req, res) => {
+    try{
+        const screening = await Screening.find();
+        res.status(201).json(screening);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+})
+
+router.get("/api/screening/auditorium:id", async (req, res) => {
+    try{
+        const auditoriumById = await Screening.find();
+        res.status(201).json(auditoriumById);
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+router.get("/api/screening")
+
+router.post("/api/screening", async (req, res) => {
     try{
         const screening = new Screening(req.body);
         await screening.save();
-        res.status(201).json(directors);
+        res.status(201).json(screening);
     } catch {
         res.status(400).json({ message: error.message });
     }
 })
 
-route.get("/api/screening", async (req, res) => {
-    try{
-        const screening = await Screening.find();
-        res.status(201).json(directors);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-})
+
+
+export default router
