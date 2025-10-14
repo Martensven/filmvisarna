c8b95f447fa08a9eb9087373324d73943293ba23
Sara <sara_lunden1@hotmail.com>
2025-10-14 09:24:32 +0200

    new secret

--- Backend/testdata/seedscript-screening.js
+++ Backend/testdata/seedscript-screening.js
@@ -5,7 +5,7 @@ import Movie from "./../routes/moviesRoute"
 
 const testdataScreening = async() => {
     try{
-
+        
     } catch (error) {
         res.status(500).json({ message: error.message })
     }
