000947afe88da59c878302ba908a2248f51f22b0
Alex Vong <avong4@gmail.com>
2025-10-16 13:22:45 +0200

    added some validation to ticketType schema

--- Backend/models/ticketTypeSchema.js
+++ Backend/models/ticketTypeSchema.js
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
 
 const ticketTypeSchema = new mongoose.Schema({
-    ticketName: { type: String, required: true },
-    price: { type: Number, required: true }
+    ticketName: { type: String, required: true, enum: ['Vuxen', 'Senior', 'Barn'] },
+    price: { type: Number, required: true, min: 0 }
 });
 
 export const TicketType = mongoose.model('Ticket', ticketTypeSchema);
