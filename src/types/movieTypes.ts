08b8e30ca2e782a3eda519874a579e406bc377f4
Jesper Eriksson Stenberg <jespermail@gmail.com>
2025-10-23 11:42:47 +0200

    Types for addmovieform

---
+++ src/types/movieTypes.ts
@@ -0,0 +1,40 @@
+export interface Genre {
+    _id: string;
+    title: string;
+}
+
+export interface Actor {
+    _id: string;
+    name: string;
+}
+
+export interface Director {
+    _id: string;
+    name: string;
+}
+
+export interface Distributor {
+    _id: string;
+    name: string;
+}
+
+export interface Theme {
+    _id: string;
+    themeDesc: string;
+}
+
+export interface MovieInput {
+    title: string;
+    imageSrc: string;
+    releaseYear: number;
+    age: number;
+    length: number;
+    description: string;
+    youtubeTrailers: string;
+    genres: string[];
+    actors: string[]; 
+    directors: string[];
+    distributor: string[];
+    themes: string[];
+    scheduleType: "smallTheater" | "bigTheater";
+}
