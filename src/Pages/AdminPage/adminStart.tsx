98755a262430b2c331ff25e59272b67bbb1aa815
Jesper Eriksson Stenberg <jespermail@gmail.com>
2025-11-02 00:15:11 +0100

    movie type added in adminTypes
    movie type added in adminTypes for use in adminStart

--- src/Pages/AdminPage/adminStart.tsx
+++ src/Pages/AdminPage/adminStart.tsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import { useNavigate } from "react-router";
+import type { Movie } from "../../types/adminTypes.ts";
 
 
 export default function AdminStart() {
+
   // Today's date in YYYY-MM-DD format Split at T to remove time portion
   const [date, setDate] = useState(() => new Date().toISOString().slice(0, 10));
-  const [movies, setMovies] = useState<any[]>([]);
+  const [movies, setMovies] = useState<Movie[]>([]);
   const nav = useNavigate();
 
 
---
+++ src/types/adminTypes.ts
@@ -0,0 +1,7 @@
+// Movie type used in admin pages
+export type Movie = {
+  _id: string;
+  title: string;
+  imageSrc: string;
+  releaseYear: number;
+}
