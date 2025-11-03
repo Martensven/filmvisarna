846e9203218c6463a79667f803000caade80f092
Jesper Eriksson Stenberg <jespermail@gmail.com>
2025-11-03 09:14:46 +0100

    Added a type for screeningitems
    Added a type for screeningitems

--- src/Pages/AdminPage/adminScreening.tsx
+++ src/Pages/AdminPage/adminScreening.tsx
@@ -1,8 +1,17 @@
+type ScreeningItem = {
+  id: string;
+  movieTitle: string;
+  time: string;
+  auditorium: string;
+  bookedCount: number;
+  totalSeats: number;
+};
+
 export default function AdminScreenings() {
-    return (
-        <section>
-            <h2>Hantera Filmvisningar</h2>
-            <p>Här kan du lägga till, redigera eller ta bort filmvisningar.</p>
-        </section>
-    );
+  return (
+    <section>
+      <h2>Hantera Filmvisningar</h2>
+      <p>Här kan du lägga till, redigera eller ta bort filmvisningar.</p>
+    </section>
+  );
 }
