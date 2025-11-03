9cf8ee8b8e0afdeff4b379d3d8d16d0e3dcd995b
Sara <sara_lunden1@hotmail.com>
2025-11-03 14:55:08 +0100

    Loggo effect
    Loggo build with flickering effects

--- src/Components/header/LoggoComponent.tsx
+++ src/Components/header/LoggoComponent.tsx
@@ -4,9 +4,15 @@ import "./LoggoStylesheet.css"
 export default function LoggoComponent(){
 
     return(
-        <main className="Loggo-box">
-            <h1 className="">FILMVISARNA</h1>
-            <h2 className="">FILMER FRÅN FÖRR</h2>
+        <main className="Loggo-box ml-30 sticky relative">
+            <h1 className="h1-font m-0 p-0 absolute top-6
+            lg:text-2xl">FILMVISARNA</h1>
+            <h2 className="h2-font flex flex-row m-0 p-0 text-red-200 absolute top-12">
+                <div className="flicker-fast-effect">F</div>
+                ILMER 
+                <div className="flicker-slow-effect ml-1 ">F</div>
+                RÅN <div className="flicker-fast-effect ml-1">F</div>
+                ÖRR</h2>
         </main>
     )
 }
