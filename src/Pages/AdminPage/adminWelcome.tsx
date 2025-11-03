508afa34e7e7422c4ab848b9b70da53b8a552b4e
Jesper Eriksson Stenberg <jespermail@gmail.com>
2025-11-03 09:10:21 +0100

    Added welcome user component
    Added welcome user component on startpage

--- src/Pages/AdminPage/adminWelcome.tsx
+++ src/Pages/AdminPage/adminWelcome.tsx
@@ -1,5 +1,16 @@
+// Imort useAuth hook to get user info
+import { useAuth } from "../../context/authContext";
 export default function AdminWelcome() {
+    const { user, loading } = useAuth();
+
+    if (loading) return <p>Laddar...</p>;
+    if (!user) return <p>Åtkomst nekad. Du måste vara inloggad som administratör.</p>;
+
     return (
-      <h1>Hej Admin</h1>
+      <>
+        <section className="text-center h-fit bg-[#243365] flex items-center justify-center p-8 rounded-xl">
+            <h2 className="text-xl font-bold">Välkommen, {user.firstName}!</h2>
+        </section>
+      </>
     );
 }
