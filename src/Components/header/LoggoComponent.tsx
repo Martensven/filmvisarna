c3f944efeb37360b552e62f3d6359fd663d327f7
Sara <sara_lunden1@hotmail.com>
2025-11-03 15:15:34 +0100

    Responsive work on loggo

--- src/Components/header/LoggoComponent.tsx
+++ src/Components/header/LoggoComponent.tsx
@@ -4,10 +4,19 @@ import "./LoggoStylesheet.css"
 export default function LoggoComponent(){
 
     return(
-        <main className="Loggo-box ml-30 sticky relative">
-            <h1 className="h1-font m-0 p-0 absolute top-6
-            lg:text-2xl">FILMVISARNA</h1>
-            <h2 className="h2-font flex flex-row m-0 p-0 text-red-200 absolute top-12">
+        <main className="Loggo-box ml-10 w-42 h-22 relative
+        xs:w-40 xs:h-20 xs:ml-5
+        sm:ml-10
+        md:ml-10 md:w-46 md:h-24
+        lg:ml-15">
+            <h1 className="h1-font m-0 p-0 absolute top-5 text-lg
+            xs:text-base xs:top-5 
+            md:text-lg md:top-7 
+            lg:text-2xl lg:top-6">FILMVISARNA</h1>
+            <h2 className="h2-font flex flex-row m-0 p-0 text-red-200 absolute top-11 text-xs
+            xs:text-xs xs:top-10
+            md:text-sm md:top-12  
+            lg:top-12">
                 <div className="flicker-fast-effect">F</div>
                 ILMER 
                 <div className="flicker-slow-effect ml-1 ">F</div>
