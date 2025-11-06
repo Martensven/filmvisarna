91d7084885b79e7db369cbccb03a66375d0ba821
Emil Asikainen <emilasikainen@hotmail.com>
2025-11-06 13:52:14 +0100

    generate unique bookingNumber with pre-save hook

--- Backend/models/bookingSchema.js
+++ Backend/models/bookingSchema.js
@@ -5,6 +5,9 @@ const bookingSchema = new mongoose.Schema({
     // If user is logged in, reference their userId. If not, store as null and use guest info.
     user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: false, default: null },
 
+    // unique booking reference
+    bookingNumber: { type: String, required: false, unique: true },
+
     // screening info
     screening_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Screening', required: false, default: null },
 
@@ -32,6 +35,32 @@ const bookingSchema = new mongoose.Schema({
 
     // booking timestamp
     created_at: { type: Date, default: Date.now }
+});
+
+// Function to generate a unique booking number like ABC123
+const generateBookingNumber = () => {
+    const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const digits = '0123456789';
+    const randomLetters = Array.from({ length: 3 }, () => letters.charAt(Math.floor(Math.random() * letters.length))).join('');
+    const randomDigits = Array.from({ length: 3 }, () => digits.charAt(Math.floor(Math.random() * digits.length))).join('');
+    return `${randomLetters}${randomDigits}`;
+}
+
+// Pre-save hook to set unique booking number
+bookingSchema.pre('save', async function (next) {
+    if (!this.bookingNumber) {
+        console.log("Genererar nytt bokningsnummer");
+        let unique = false;
+        while (!unique) {
+            const newBookingNumber = generateBookingNumber();
+            const existingBooking = await mongoose.models.Booking.findOne({ bookingNumber: newBookingNumber });
+            if (!existingBooking) {
+                this.bookingNumber = newBookingNumber;
+                unique = true;
+            }
+        }
+    }
+    next();
 });
 
 export const Booking = mongoose.model('Booking', bookingSchema);
