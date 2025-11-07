e23715c30eda4117cb1b4f26a2855bb75423a93e
Sara <sara_lunden1@hotmail.com>
2025-11-07 10:32:55 +0100

    new recipe syle

--- src/Pages/BookingPage/components/CheckoutRecipe.tsx
+++ src/Pages/BookingPage/components/CheckoutRecipe.tsx
@@ -21,12 +21,12 @@ export default function CheckoutRecipe() {
 
 
   return (
-    <aside className="flex
-    sm:w-11/12 sm:flex sm:flex-row sm:justify-center sm:gap-2 sm:items-center 
+    <aside className="flex flex-col justify-between items-center w-11/12 
+    sm:w-11/12 sm:flex sm:flex-row sm:justify-center sm:gap-1 sm:items-center 
     md:w-11/12 md:flex-col">
-      <section className="Amount-of-tickets flex flex-row justify-between items-center w-72 h-auto border-t mt-1 border-gray-400
+      <section className="Amount-of-tickets flex flex-row justify-between items-center w-full h-auto 
       sm:w-42 
-      md:w-66 ">
+      md:w-66 md:border-t md:mt-1 md:border-gray-400 ">
         <h2 className="text-[#e4e1e1] text-sm m-1  md:text-base md:m-5">
           Antal biljetter:
         </h2>
@@ -35,28 +35,25 @@ export default function CheckoutRecipe() {
         </p>
       </section>
 
-      <section className="Type-of-tickets flex flex-row justify-between items-center w-70 h-auto border-t border-gray-400 text-sm
+      <section className="Type-of-tickets flex flex-row grid-2 justify-between items-center w-full h-auto text-sm border-t border-b border-gray-400 mx-2 my-2
       sm:w-72 sm:text-sm
-      md:flex-col md:p-5 md:text-base md:w-66">
-        <span className="flex justify-between space m-1
+      md:flex-col md:p-5 md:text-base md:w-66 md:border-t md:border-gray-400 ">
+        <span className="flex justify-center space w-9/12 m-1  
         md:w-full">
-          <h2 className="mr-1">barn: </h2>
-          <p>{childTotalPrice} kr</p>
+          <h2 className="">barn:  {childTotalPrice} kr</h2>
         </span>
-        <span className="flex justify-between space
+        <span className="flex justify-center space w-9/12 m-1 
         md:w-full">
-          <h2 className="mr-1">senior:  </h2>
-          <p>{seniorTotalPrice} kr</p>
+          <h2 className="">senior:  {seniorTotalPrice} kr</h2>
         </span>
-        <span className="flex justify-between space
+        <span className="flex justify-center space w-9/12 m-1    
         md:w-full">
-          <h2 className="mr-1">vuxen: </h2>
-          <p>{adultTotalPrice} kr</p>
+          <h2 className="">vuxen:  {adultTotalPrice} kr</h2>
         </span>
       </section>
-      <section className="Total flex flex-row justify-between items-center w-72 h-auto border-t mt-1 border-gray-400
+      <section className="Total flex flex-row justify-between items-center w-full h-auto 
       sm:w-36
-      md:w-66">
+      md:w-66 md:border-t md:mt-1 ">
         <h2 className="text-[#e4e1e1] text-sm m-1  
         md:text-base md:m-5">
           Totalt:
