169b197fc8c620a20006e578cda38b22e8d8c799
Martensven <martengullstrom@gmail.com>
2025-11-07 12:38:46 +0100

    updated responsiveness of CheckoutRecipe

--- src/Pages/BookingPage/components/CheckoutRecipe.tsx
+++ src/Pages/BookingPage/components/CheckoutRecipe.tsx
@@ -14,19 +14,19 @@ export default function CheckoutRecipe() {
   return (
     <>
       <aside
-        className="flex h-auto
-    sm:w-11/12 sm:flex sm:flex-row sm:justify-center sm:gap-2 sm:items-center 
-    md:w-11/12 md:flex-col"
+        className="flex h-auto 
+    w-11/12 sm:justify-center sm:gap-2 items-center 
+    md:w-11/12 flex-col"
       >
-        <section className="flex flex-row justify-between w-70 h-auto border-t mt-5 p-1 border-gray-400 text-sm
-      sm:w-72 sm:text-sm
-      md:text-base md:w-66 py-3">
-          <p className="">
-            Antal biljetter:
+        <section className="Type-of-tickets flex justify-between items-center w-70 h-auto border-t border-gray-400 text-sm
+      sm:w-72 sm:text-sm md:flex-row
+       md:p-1 md:text-base md:w-66 pt-3 pb-3">
+          <p className="text-[#e4e1e1] text-xs md:text-base">
+            Biljetter:
           </p>
-          <p>{totalTickets} st</p>
+          <p className="text-[#e4e1e1] text-xs md:text-base">{totalTickets} st</p>
         </section>
-        <section className="flex items-center w-70 h-auto border-t border-gray-400 text-sm
+        <section className="flex items-center justify-between w-70 h-auto border-t border-gray-400 text-sm
       sm:w-72 sm:text-sm
       md:flex-col  md:text-base md:w-66">
           {/* Visa varje biljettyp */}
@@ -37,28 +37,28 @@ export default function CheckoutRecipe() {
             return (
               <div
                 key={type._id}
-                className="flex justify-between items-center
+                className="flex md:flex-row justify-between items-center 
         md:w-full h-10 my-1"
               >
-                <p className="text-[#e4e1e1] text-sm  md:text-base m-1">{type.ticketName}:</p>
+                <p className="text-[#e4e1e1] text-xs  md:text-base m-1">{type.ticketName}:</p>
                 {/* <p className="text-[#e4e1e1] text-sm m-2  md:text-base md:m-5 flex justify-center">
                 {quantity} st × {type.price} kr
               </p> */}
-                <p className="text-[#e4e1e1] text-sm  md:text-base">{subtotal} kr</p>
+                <p className="text-[#e4e1e1] text-xs  md:text-base">{subtotal} kr</p>
               </div>
             );
           })}
         </section>
 
         {/* Sammanställning */}
-        <section className="Type-of-tickets flex flex-row justify-between items-center w-70 h-auto border-t border-gray-400 text-sm
+        <section className="Type-of-tickets flex justify-between items-center w-70 h-auto border-t border-gray-400 text-xs
       sm:w-72 sm:text-sm
-       md:p-1 md:text-base md:w-66 pt-3">
-          <h2 className="text-[#e4e1e1] text-sm md:text-base font-semibold">
+       md:p-1 md:text-base md:w-66 md:flex-row pt-3">
+          <p className="text-[#e4e1e1] text-xs md:text-base">
             Totalt:
-          </h2>
+          </p>
 
-          <p className="text-[#e4e1e1] text-sm md:text-base font-bold">
+          <p className="text-[#e4e1e1] text-xs md:text-base ">
             {totalPrice} kr
           </p>
         </section>
