fa0e32d0a4843ed84a93e74d419ef23e576f4250
Jesper Eriksson Stenberg <jespermail@gmail.com>
2025-11-20 14:35:19 +0100

    Cookiepopup new style
    new style on cookie popup

--- src/Components/CookiePopup/cookiePopup.tsx
+++ src/Components/CookiePopup/cookiePopup.tsx
@@ -31,12 +31,10 @@ const CookiePopup = () => {
   if (!showPopup) return null;
 
   return (
-    <div
-      className="fixed inset-0 bg-black/50 backdrop-blur-sm z-[1000] flex items-end justify-center sm:items-center"
-    >
-      <div
-        className="bg-gray-800 text-white p-6 rounded-xl shadow-2xl w-[90%] max-w-md animate-fadeInUp"
-      >
+    <div className="fixed inset-0 bg-black/50 backdrop-blur-sm z-[1000] flex items-center justify-center p-4">
+
+      <div className="popOut-box bg-amber-50 w-full max-w-md inset-shadow-sm inset-shadow-[#8a6a0094] p-4 rounded-md shadow-md text-gray-950">
+
         <h3 className="text-lg font-semibold mb-3">Cookies inställningar</h3>
         <p className="text-sm mb-4 leading-relaxed">
           Filmsmedjan använder cookies för inloggning och användarsessioner. Filmsmedjan använder för nuvarande inte cookies
@@ -73,7 +71,7 @@ const CookiePopup = () => {
                 onChange={(e) => setAnalyticsConsent(e.target.checked)}
                 className="sr-only peer"
               />
-              <div className="w-11 h-6 bg-gray-500 rounded-full peer peer-checked:bg-blue-600 transition"></div>
+              <div className="w-11 h-6 bg-gray-500 rounded-full peer peer-checked:bg-[#243365] transition"></div>
               <span
                 className={`absolute left-[2px] top-[2px] w-5 h-5 bg-white rounded-full transition-transform ${
                   analyticsConsent ? "translate-x-5" : ""
@@ -86,7 +84,7 @@ const CookiePopup = () => {
         <div className="flex justify-center">
           <button
             onClick={handleSavePreferences}
-            className="bg-blue-600 hover:bg-blue-700 transition px-4 py-2 rounded-md text-sm"
+            className="bg-[#243365] hover:bg-[#1e2a4d] text-white transition px-4 py-2 rounded-md text-sm cursor-pointer"
           >
             Spara inställningar
           </button>
