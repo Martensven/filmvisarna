f11587474796c6b3ea1080384338e653bfd3562b
[Luckmore83] <[daaksve@gmail.com]>
2025-11-20 09:49:29 +0100

    png images that needed converting done, also changed name to Filmsmedjan

--- src/Pages/AboutPage/aboutPage.tsx
+++ src/Pages/AboutPage/aboutPage.tsx
@@ -8,25 +8,25 @@ export default function AboutPage() {
     <article className="flex flex-col xl:flex-row items-center md:items-start">
       <img src="/images/movietheater.jpg" alt="biosalong" className="w-full h-auto object-contain rounded-md md:w-[500px] md:h-[350px] md:object-cover"/>
       <p className="m-5 md:m-5">
-        Filmvisarna grundades i den charmiga staden Småstad, där bio alltid har varit mer än bara underhållning, det har varit en samlingsplats för drömmare, familjer och filmälskare i generationer. Idén föddes av en grupp passionerade cineaster som delade en och samma vision: att bevara den magi som klassisk film en gång gav publiken.
+        Filmsmedjan grundades i den charmiga staden Småstad, där bio alltid har varit mer än bara underhållning, det har varit en samlingsplats för drömmare, familjer och filmälskare i generationer. Idén föddes av en grupp passionerade cineaster som delade en och samma vision: att bevara den magi som klassisk film en gång gav publiken.
       </p>
     </article>
 
     <article className="flex flex-col xl:flex-row items-center md:items-start">
         <img src="/images/projector2.png" alt="filmprojektor" className="w-full h-auto object-contain rounded-md md:w-[500px] md:h-[350px] md:object-cover"/>
         <p className="m-5">
-        I en tid då moderna blockbusters och digitala effekter dominerar ville Filmvisarna skapa något annorlunda, ett levande minnesrum för filmens guldår. Vår kärlek sträcker sig från de tidiga svartvita stumfilmerna från 1910-talet, genom Hollywoods gyllene era, 70- och 80-talets kultklassiker, ända fram till de ikoniska 90-talsfilmerna som formade en hel generation.
+        I en tid då moderna blockbusters och digitala effekter dominerar ville Filmsmedjan skapa något annorlunda, ett levande minnesrum för filmens guldår. Vår kärlek sträcker sig från de tidiga svartvita stumfilmerna från 1910-talet, genom Hollywoods gyllene era, 70- och 80-talets kultklassiker, ända fram till de ikoniska 90-talsfilmerna som formade en hel generation.
         </p>
     </article>
 
     <article className="flex flex-col xl:flex-row items-center md:items-start">
       <img src="/images/popcorn2.jpg" alt="Popkorn" className="w-full h-auto object-contain rounded-md md:w-[500px] md:h-[350px] md:object-cover"/>
       <p className="m-5">
-        Filmvisarna är mer än en biograf, det är en tidsmaskin. Vi brinner för att väcka nostalgiska känslor och låta publiken återuppleva filmhistoriens pärlor precis som de var tänkta att ses: med gemenskap, skratt, spänning och applåder i salongen.
+        Filmsmedjan är mer än en biograf, det är en tidsmaskin. Vi brinner för att väcka nostalgiska känslor och låta publiken återuppleva filmhistoriens pärlor precis som de var tänkta att ses: med gemenskap, skratt, spänning och applåder i salongen.
 
         Oavsett om det är en storslagen matiné, en kultklassiker på kvällstid eller en familjevänlig nostalgifilm på helgen, strävar vi efter att skapa en unik och varm filmupplevelse. Vi vill bevara filmarvet, hylla historiens mästerverk och föra dem vidare till nya generationer.
 
-        För hos Filmvisarna handlar det inte bara om att titta på film, det handlar om att minnas, känna och uppleva film på riktigt.
+        För hos Filmsmedjan handlar det inte bara om att titta på film, det handlar om att minnas, känna och uppleva film på riktigt.
       </p>
     </article>
 
